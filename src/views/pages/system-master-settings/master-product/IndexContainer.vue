a4c8195036487ea8ac030d258185642067754c0d
Your Name <you@example.com>
2023-06-04 22:48:25 +0700

    isssue

--- src/views/pages/system-master-settings/master-product/IndexContainer.vue
+++ src/views/pages/system-master-settings/master-product/IndexContainer.vue
@@ -1,34 +1,32 @@
 <template>
-  <VCard class="mb-6 pa-2">
-    <VTabs
-      v-model="userTab"
-      class="v-tabs-pill"
-    >
-      <VTab
-        v-for="tab in tabs"
-        :key="tab.icon"
-        :to="tab.to"
+  <div>
+    <VCard class="mb-6 pa-2">
+      <VTabs
+        v-model="userTab"
+        class="v-tabs-pill"
       >
-        <VIcon
-          :size="18"
-          :icon="tab.icon"
-          class="me-1"
-        />
-        <span>{{ tab.title }}</span>
-      </VTab>
-    </VTabs>
-  </VCard>
-  <Transition>
+        <VTab
+          v-for="tab in tabs"
+          :key="tab.icon"
+          :to="tab.to"
+        >
+          <VIcon
+            :size="18"
+            :icon="tab.icon"
+            class="me-1"
+          />
+          <span>{{ tab.title }}</span>
+        </VTab>
+      </VTabs>
+    </VCard>
     <RouterView class="productsrouter" />
-  </Transition>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import { useRoute } from 'vue-router'
 
 const userTab = ref(null)
-const route = useRoute()
 
 const tabs = [
   {
