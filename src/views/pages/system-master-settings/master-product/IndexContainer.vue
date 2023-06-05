e6e1e2cbdb0966a58484a4619745a26b6f3509e9
Your Name <you@example.com>
2023-06-05 16:01:11 +0700

    123

--- src/views/pages/system-master-settings/master-product/IndexContainer.vue
+++ src/views/pages/system-master-settings/master-product/IndexContainer.vue
@@ -1,24 +1,61 @@
 <template>
   <div>
-    <VCard class="mb-6 pa-2">
-      <VTabs
-        v-model="userTab"
-        class="v-tabs-pill"
-      >
-        <VTab
-          v-for="tab in tabs"
-          :key="tab.icon"
-          :to="tab.to"
+    <section class="hidden-md-and-up">
+      <VCard class="mb-6 pa-3">
+        <IconBtn
+          id="vertical-nav-toggle-btn"
+          class=""
+          @click="drawer = !drawer"
         >
           <VIcon
-            :size="18"
-            :icon="tab.icon"
-            class="me-1"
+            size="26"
+            icon="tabler-menu-2"
           />
-          <span>{{ tab.title }}</span>
-        </VTab>
-      </VTabs>
-    </VCard>
+        </IconBtn>
+        <VNavigationDrawer
+          v-model="drawer"
+          app
+          class="drawer-container"
+        >
+          <template
+            v-for="tab in tabs"
+            :key="tab.icon"
+          >
+            <VListItem :to="tab.to">
+              <VListItemIcon>
+                <VIcon :size="18">
+                  {{ tab.icon }}
+                </VIcon>
+              </VListItemIcon>
+              <VListItemContent>
+                <VListItemTitle>{{ tab.title }}</VListItemTitle>
+              </VListItemContent>
+            </VListItem>
+          </template>
+        </VNavigationDrawer>
+      </VCard>
+    </section>
+    <section class="hidden-sm-and-down">
+      <VCard class="mb-6 pa-2">
+        <VTabs
+          v-model="userTab"
+          class="v-tabs-pill"
+        >
+          <VTab
+            v-for="tab in tabs"
+            :key="tab.icon"
+            :to="tab.to"
+          >
+            <VIcon
+              :size="18"
+              :icon="tab.icon"
+              class="me-1"
+            />
+            <span>{{ tab.title }}</span>
+          </VTab>
+        </VTabs>
+      </VCard>
+    </section>
     <RouterView class="productsrouter" />
   </div>
 </template>
@@ -27,6 +64,7 @@
 import { ref } from 'vue'
 
 const userTab = ref(null)
+const drawer = ref(false)
 
 const tabs = [
   {
@@ -51,3 +89,9 @@ const tabs = [
   },
 ]
 </script>
+
+<style scoped>
+.drawer-container {
+  z-index: 9999;
+}
+</style>
